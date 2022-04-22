16586c857e06003d090bb8a5aa26ac22cf86dde1
Mohammed Naji <moha.buy@gmail.com>
2022-04-22 22:44:49 +0400

    add line

--- source/routes/system/index.ts
+++ source/routes/system/index.ts
@@ -4,4 +4,5 @@ import { systemControllers } from "@controllers";
 const router = express.Router();
 router.get("/ping", systemControllers.health.checkHealth);
 router.use("/system",router);
+
 export default router;
